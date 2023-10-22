28e5881172bc5dc0e2b38b1e34e87e0195ffd0c8
Harley <harley201009@hotmail.com>
2023-10-22 13:18:27 -0300

    Adding bulma and fontawesome

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
-import './assets/main.css'
+// import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import "@fortawesome/fontawesome-free/css/all.css";
 
 const app = createApp(App)
 
