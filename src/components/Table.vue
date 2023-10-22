0d4e0dbac9d7f8e960f41793f6822c26af158bd5
Harley <harley201009@hotmail.com>
2023-10-22 13:21:09 -0300

    Creating component Table and TableFilter

---
+++ src/components/Table.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <table class="table">
+      <thead>
+        <tr>
+          <th v-for="(header, index) in tableHeaders" :key="index">{{ header }}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(row, rowIndex) in tableData" :key="rowIndex">
+          <td v-for="(cell, cellIndex) in row" :key="cellIndex">{{ cell }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    tableHeaders: Array, // Array de cabeçalhos da tabela
+    tableData: Array // Array de dados da tabela
+  }
+}
+</script>
+
+<style>
+table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+th,
+td {
+  border: 1px solid #ccc;
+  padding: 8px;
+  text-align: left;
+}
+</style>
---
+++ src/components/TableFilter.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="field is-grouped">
+    <div class="control">
+      <button class="button is-dark">Novo</button>
+    </div>
+    <div class="control has-icons-left has-icons-right">
+      <input class="input" type="text" placeholder="Filtro" v-model="filter" />
+      <span class="icon is-small is-left">
+        <i class="fas fa-search search"></i>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { watch } from 'vue';
+import { ref, defineEmits } from 'vue';
+
+const filter = ref("");
+const emit = defineEmits(['filter-updated']);
+
+watch(filter, () => {
+  emit('filter-updated', filter.value);
+});
+</script>
