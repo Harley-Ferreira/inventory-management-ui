c68b4ef15a3eaa61a952ad01dc12a514dec3a327
Harley <harley201009@hotmail.com>
2023-10-22 13:20:19 -0300

    Creating Navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,11 @@
+<template>
+  <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
+    <div style="margin-left: 40%" class="navbar-menu">
+      <div class="navbar-start">
+        <router-link to="/product" class="navbar-item">Produtos</router-link>
+        <router-link to="/supplier" class="navbar-item">Fornecedores</router-link>
+        <router-link to="/transaction" class="navbar-item">Transações</router-link>
+      </div>
+    </div>
+  </nav>
+</template>
