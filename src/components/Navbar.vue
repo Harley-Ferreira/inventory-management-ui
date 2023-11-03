f79a8c1875fd696212be6d5b36b2af3a917d4df2
Harley <harley201009@hotmail.com>
2023-11-02 22:36:41 -0300

    Making some adjustment to the navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,23 @@
 <template>
   <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
-    <div style="margin-left: 40%" class="navbar-menu">
+    <div class="navbar-brand">
+      <a class="navbar-item">
+        <img src="/src/assets/im-logo.png" width="60" height="30" />
+      </a>
+
+      <a role="button"
+        class="navbar-burger"
+        aria-label="menu"
+        aria-expanded="false"
+        data-target="navbarBasicExample"
+      >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+    
+    <div class="navbar-menu">
       <div class="navbar-start">
         <router-link to="/product" class="navbar-item">Produtos</router-link>
         <router-link to="/supplier" class="navbar-item">Fornecedores</router-link>
